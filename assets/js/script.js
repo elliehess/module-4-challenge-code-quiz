be73d12975299e8e3b5c8c9cd9f3515010e109b5
Ellie <ellie@Ellies-MacBook-Pro.local>
2022-12-17 21:59:32 -0300

    quiz questions

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+
+
+
+
+
+
+
+
+
+
+//What does CSS stand for?
+//Computer Style Sources
+//Color Style Sheets
+//Cascading Style Sheets CORRECT
+//Computer Stance Sources
+
+//Arrays in Javascript can be used to store:
+//Booleans
+//Strings
+//Numbers
+//All of the above CORRECT
+
+//What does HTML stand for?
+//HyperText Markup Language CORRECT
+//Hyperfixated Made Language
+//Hyper Mode Load
+//HowTo Make Language
+
+//A Boolean value can be:
+//true
+//false
+//Both a and b CORRECT
+//Neither a or b 
+
+//Javascript is used by programmers to:
+//style a presentation of documents 
+//create dynamic and interactive web content CORRECT
+//create static web pages and applications 
+//none of the above
+
+
+
+
+//submit button
+//go back
+//clear high scores
+//set time interval
+//local.storage for high scores
